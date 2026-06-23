ba0202fd3ba07a98095463190880dcf2e8d8d96f
Nilkantha Dwibedi [L-T-F16] <nilkanthadwibedi66@gmail.com>
2026-06-23 20:46:20 +0530

    Auth fix-> preventing logged in user to navigate to /login, /signup------> if logged in and try to go to /login,/signup then navigate to /

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { AuthLayout } from '../features/auth/AuthLayout';
 import { LoginForm } from '../features/auth/components/LoginForm';
+import { useAuthStore } from '../store/auth.store';
+import { Navigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
+  const { user } = useAuthStore();
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
   return (
     <AuthLayout>
       <LoginForm />
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { AuthLayout } from '../features/auth/AuthLayout';
 import { SignupForm } from '../features/auth/components/SignupForm';
+import { useAuthStore } from '../store/auth.store';
+import { Navigate } from 'react-router-dom';
 
 const SignupPage: React.FC = () => {
+  const { user } = useAuthStore();
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
   return (
     <AuthLayout>
       <SignupForm />
